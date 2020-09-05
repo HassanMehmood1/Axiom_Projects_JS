b21104880625c1a8020feaf1bcc9eda5e0fa286e
Hassan Mehmood <hmehmood411@gmail.com>
2020-09-05 15:37:00 +0500

    Update script.js
    Added functionality to check email validation, check length and also check password match validation.

--- Proj_01/script.js
+++ Proj_01/script.js
@@ -20,9 +20,13 @@ function showSuccess(input){
     formControl.className = 'form-control success';
 };
 //Function to check if email is valid
-function isValidEmail(email){
+function checkEmail(input){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())){
+        showSuccess(input);
+    } else {
+        showError(input,`Please provide a valid email`)
+    }
 };
 // Functions to check if required field have data
 function checkRequired(inputArray){
@@ -34,6 +38,25 @@ function checkRequired(inputArray){
         }
     });
 };
+//Function to check length of input fields
+function checkLength(input,min,max){
+    if (input.value.length < min){
+        showError(input,`${getFieldId(input)} needs to be atleast ${min} characters`);
+    } else if (input.value.length > max){
+        showError(input,`${getFieldId(input)} needs to be less than ${max} characters`);
+    } else{
+        showSuccess(input);
+    }
+};
+//Function to check if password and confirm password is matched
+function checkPasswordsMatch(input1,input2){
+    if(input1.value !== input2.value){
+        showError(input2,"Passwords don't match")
+    }
+};
+
+
+
 //Functions to get the id of the input field with proper case
 function getFieldId(input){
     return input.id.charAt(0).toUpperCase()+input.id.slice(1);
@@ -42,5 +65,9 @@ function getFieldId(input){
 // this is an event listener for the form on 
 form.addEventListener('submit',function( e){
     e.preventDefault();
-   checkRequired([username,email, password,password2])
+   checkRequired([username,email, password,password2]);
+   checkLength(username,3,10);
+   checkLength(password,6,30);
+   checkEmail(email);
+   checkPasswordsMatch(password,password2);
 });
