6358395590493603960f9da553fc915e71da582e
Hassan Mehmood <hmehmood411@gmail.com>
2020-10-13 10:07:51 +0500

    High Order Array Methods
    Creat UI and Implement whole project using JS to complete the functionality.

---
+++ Proj_05/script.js
@@ -0,0 +1,105 @@
+
+//Getting DOM Element
+const main = document.getElementById('main');
+const addUserButton = document.getElementById('add-user');
+const doubleMoneyButton = document.getElementById('double');
+const showMillionairesButton = document.getElementById('show-millionaires');
+const sortButton = document.getElementById('sort');
+const totalButton = document.getElementById('calculate-total');
+
+//Initialize Data Array
+let data = [];
+
+//Create Initial Users
+generateRandomUser();
+generateRandomUser();
+
+//Function to Fetch Random User from API
+// API: randomuser.me/api
+async function generateRandomUser(){
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+
+    const user = data.results[0];
+
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        worth: Math.round(Math.random()*1000000)
+    }
+    addData(newUser);
+    
+}
+
+//Function to Double the Net Worth of Each User
+function doubleWorth(){
+    data = data.map( item => {
+        return { ...item, worth: item.worth*2 }
+    } );
+    updateDOM();
+};
+
+// Function to sort the User by Richest Users
+function sortRichest(){
+    data.sort( (a,b) => b.worth - a.worth);
+
+    updateDOM();
+};
+//Functions to filter the users and only shows millionaires
+function showMillionaires(){
+    data = data.filter(
+        item => item.worth > 1000000
+    );
+    updateDOM();
+};
+
+//Function to calculate the total networth of all the users
+function calculateTotalNetWorth(){
+    const totalWorth = data.reduce(
+        (acc, item) => (acc += item.worth), 0
+        );
+            const totalNetWorthElement = document.createElement('div');
+            totalNetWorthElement.innerHTML = `<h3>Total NetWorth: <strong>${formatCurrency(totalWorth)}</strong></h3>`;
+            main.appendChild(totalNetWorthElement);
+};
+
+// Add Newly Generated User into the Data Array
+function addData(newUser){
+    data.push(newUser);
+
+
+    updateDOM();
+};
+
+// Function Update The UI with DOM
+function updateDOM(inputData = data){
+    main.innerHTML = '<h2><strong>Name</strong>Net Worth</h2>';
+
+    inputData.forEach( item => {
+        const element = document.createElement('div');
+        element.classList.add('name');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatCurrency(item.worth)}`;
+        main.appendChild(element); 
+    } );
+}
+
+//Function to Format a number as a currrency
+function formatCurrency(num){
+    return 'PKR '+ (num).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+//Event Listeners 
+// 1. Add User Event Listener
+addUserButton.addEventListener('click', generateRandomUser);
+
+//2. Add Double Money Event Listener
+doubleMoneyButton.addEventListener('click', doubleWorth);
+
+//3. Add sort Event Listener
+sortButton.addEventListener('click', sortRichest);
+
+//4. Add Show Millionaire Event Listener
+showMillionairesButton.addEventListener('click', showMillionaires);
+
+//5. Add Calculate Total Wealth Event Listener
+totalButton.addEventListener('click', calculateTotalNetWorth);
